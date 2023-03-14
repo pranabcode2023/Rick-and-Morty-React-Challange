37d16c3e1cb704e1fbaef2835c1f2d2ee4b07541
pranabcode2023 <pranabcode2023@gmail.com>
2023-03-14 16:17:02 +0100

    my first commit

---
+++ react-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FlipCard from './Components/cards/FlipCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first react project</h1>
+      <FlipCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-project/src/Components/cards/FlipCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function FlipCard(params) {
+
+    return (
+        <div>
+            <h2>Ricky and Morty</h2>
+        </div>
+    )
+
+}
+
+export default FlipCard;
+
